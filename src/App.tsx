95246a3eb86648372747eff933c671532bba43b7
Jesus Hernandez <111300985+JesusHernandez223258@users.noreply.github.com>
2024-04-23 07:31:42 -0600

    a
    df

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,16 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 type RegisterType = {
-  userName: string;
-  password: string;
+  id_venta: number;
+  contenido: string;
+  precio: number;
 };
 
 export const App: React.FC<{}> = () => {
   const [registerData, setRegisterData] = React.useState<RegisterType>({
-    userName: "",
-    password: "",
+    id_venta: 0,
+    contenido: "",
+    precio: 0
   });
 
   const dataRegister = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -33,21 +35,26 @@ export const App: React.FC<{}> = () => {
 
     try {
       const {
-        userName,
-        password
+        id_venta,
+        contenido,
+        precio
       } = registerData;
 
-      const response = await axios.post("https://api-1-c2.onrender.com/user", {
-        userName,
-        password
-      });
+      const response = await axios.post("https://api-eventdr.onrender.com/user",
+        {
+          id_venta,
+          contenido,
+          precio
+        }
+      );
 
       if (response) {
         console.log("Registro exitoso");
 
         setRegisterData({
-          userName: "",
-          password: ""
+          id_venta: 0,
+          contenido: "",
+          precio: 0
         });
       } else {
         console.error("Error al registrar");
@@ -101,25 +108,37 @@ export const App: React.FC<{}> = () => {
 
               <Box component="form" onSubmit={handleSubmit}>
                 <TextField
-                  name="userName"
+                  name="id_venta"
                   margin="normal"
                   fullWidth
-                  label="Nombre"
+                  label="Ingrese el ID de la transacción"
                   sx={{ mt: 2, mb: 1.5 }}
                   required
+                  type="number"
                   onChange={dataRegister}
-                  value={registerData.userName}
+                  value={registerData.id_venta}
                 />
             
                 <TextField
-                  name="password"
+                  name="contenido"
                   margin="normal"
                   fullWidth
-                  label="Password"
+                  label="Ingrese el concepto de la transferencia"
                   sx={{ mt: 2, mb: 1.5 }}
                   required
                   onChange={dataRegister}
-                  value={registerData.password}
+                  value={registerData.contenido}
+                />
+                <TextField
+                  name="precio"
+                  margin="normal"
+                  fullWidth
+                  label="Ingrese el monto de la transacción"
+                  sx={{ mt: 2, mb: 1.5 }}
+                  required
+                  type="number"
+                  onChange={dataRegister}
+                  value={registerData.precio}
                 />
                 <Stack spacing={2} direction="row" justifyContent="end">
                   <Button
